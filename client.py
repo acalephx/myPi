669a5a727e4c3f799f2f30f53e91dc8b1e9b6915
acalephx <yuanzhe.xia@gmail.com>
2018-08-23 23:15:37 +0800

    my python socket

--- client.py
+++ client.py
@@ -1,11 +1,16 @@
 import socket
 import time
 
-HOST = '192.168.199.131'
-PORT = 8001
+HOST = '192.168.2.102'
+PORT = 8002
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# print('b')
 sock.connect((HOST, PORT))
-time.sleep(2)
-sock.send(b'1')
-print(sock.recv(1024).decode())
+# print('a')
+while 1:
+    time.sleep(0.5)
+    # sock.send(b'myData')
+    print(sock.recv(1024).decode())
+
 sock.close()
+
--- server.py
+++ server.py
@@ -1,20 +1,75 @@
 import socket
 import time
 
-HOST = '192.168.199.131'
-PORT = 8001
+HOST = '192.168.2.102'
+PORT = 8002
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind((HOST, PORT))
 sock.listen(5)
-while True:
-    connection, address = sock.accept()
-    try:
-        connection.settimeout(10)
-        buf = connection.recv(1024)
-        if buf:
-            connection.send(b'welcome to server!')
-            print('Connection success!')
-        else:
-            connection.send(b'please go out!')
-    except socket.timeout:
-        print('time out')
+connection, address = sock.accept()
+while 1:
+
+    time.sleep(0.5)
+    connection.send(b'myData')
+    # print(sock.recv(1024).decode())
+
+sock.close()
+
+
+# while True:
+#     # print('b')
+#     connection, address = sock.accept()
+#     # print('a')
+#     try:
+#         connection.settimeout(10)
+#         buf = connection.recv(1024)
+#         if buf:
+#             connection.send(b'welcome to server!')
+#             print('Connection success!')
+#         else:
+#             connection.send(b'please go out!')
+#     except socket.timeout:
+#         print('time out')
+#     connection.close()
+
+#import necessary package
+
+# import socket
+# import time
+# import sys
+# import RPi.GPIO as GPIO
+# #define host ip: Rpi's IP
+# HOST_IP = "192.168.2.102"
+# HOST_PORT = 8888
+# print("Starting socket: TCP...")
+# #1.create socket object:socket=socket.socket(family,type)
+# socket_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# print("TCP server listen @ %s:%d!" %(HOST_IP, HOST_PORT) )
+# host_addr = (HOST_IP, HOST_PORT)
+# #2.bind socket to addr:socket.bind(address)
+# socket_tcp.bind(host_addr)
+# #3.listen connection request:socket.listen(backlog)
+# socket_tcp.listen(1)
+# #4.waite for client:connection,address=socket.accept()
+# socket_con, (client_ip, client_port) = socket_tcp.accept()
+# print("Connection accepted from %s." %client_ip)
+# socket_con.send("Welcome to RPi TCP server!")
+# #5.handle
+# GPIO.setmode(GPIO.BOARD)
+# GPIO.setup(11,GPIO.OUT)
+# print("Receiving package...")
+# while True:
+#     try:
+#         data=socket_con.recv(512)
+#         if len(data)>0:
+#             print("Received:%s"%data)
+#             if data=='1':
+#                 GPIO.output(11,GPIO.HIGH)
+#             elif data=='0':
+#                 GPIO.output(11,GPIO.LOW)
+#             socket_con.send(data)
+#             time.sleep(1)
+#             continue
+#     except Exception:
+#             socket_tcp.close()
+#             sys.exit(1)
