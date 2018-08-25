98093df37e6e168e041086f89f267a3bd4ae0354
acalephx <yuanzhe.xia@gmail.com>
2018-08-25 21:27:52 +0800

    what i need

--- client.py
+++ client.py
@@ -1,20 +1,43 @@
 import socket
 import time
+import sys
 
 HOST = '192.168.2.102'
 PORT = 8002
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # print('b')
-sock.connect((HOST, PORT))
-# print(b'a')
-sock.send(b'a')
-# sock.send(b'b')
-while 1:
-    time.sleep(0.5)
-    buf = sock.recv(1024).decode()
-    if buf:
-        print(buf)
-
-
-sock.close()
-
+# sock.connect((HOST, PORT))
+# # print(b'a')
+# sock.send(b'a')
+# # sock.send(b'b')
+# while 1:
+#     time.sleep(0.5)
+#     buf = sock.recv(1024).decode()
+#     if buf:
+#         print(buf)
+#
+#
+# sock.close()
+while True:
+    try:
+        print("Connecting to server @ %s:%d..." % (HOST, PORT))
+        sock.connect((HOST, PORT))
+        break
+    except Exception:
+        print("Can't connect to server,try it latter!")
+        time.sleep(1)
+        continue
+print("Please input 1 or 0 to turn on/off the led!")
+while True:
+    try:
+        data = sock.recv(512).decode()
+        if len(data) > 0:
+            print("Received: %s" % data)
+            command = raw_input()
+            sock.send(command)
+            time.sleep(0.3)
+            continue
+    except Exception:
+        sock.close()
+        sock = None
+        sys.exit(1)
--- server.py
+++ server.py
@@ -5,28 +5,27 @@ HOST = '192.168.2.102'
 PORT = 8002
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind((HOST, PORT))
-sock.listen(5)
-
+sock.listen(1)
+connection, (HOST_IP, HOST_PORT) = sock.accept()
+print("Connection accepted from %s." % HOST_IP)
+connection.send(b'Welcome to RPi TCP server!')
 while 1:
     try:
-        connection, address = sock.accept()
-        # connection.settimeout(10)
         buf = connection.recv(1024)
-        if buf == 'a':
-            connection.send(b'Start!')
-            print('Start')
-            print(buf)
-            while buf != 'b':
+        if len(buf) > 0:
+            if buf == '1':
+                connection.send(b'Start!')
+                print('Start')
                 print('working')
                 connection.send(b'myData')
                 time.sleep(0.5)
-                # buf = connection.recv(1024)
-        elif buf == 'b':
-            connection.send(b'Stop!')
-            print('Stop')
-        else:
-            connection.send(b'please go out!')
-    except socket.timeout:
-        print('time out')
-connection.close()
-sock.close()
+            elif buf == '0':
+                connection.send(b'Stop!')
+                print('Stop')
+            time.sleep(0.5)
+            continue
+    except Exception:
+        sock.close()
+        # sys.exit(1)
+# connection.close()
+# sock.close()
